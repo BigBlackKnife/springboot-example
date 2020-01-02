68aad390adee908e3faca06166c1d6864f347957
BigBlackKnife <2767026270@qq.com>
2020-01-03 00:19:37 +0800

    ServletComponentScan注解

--- src/main/java/com/blaife/SpringbootExampleApplication.java
+++ src/main/java/com/blaife/SpringbootExampleApplication.java
@@ -3,13 +3,20 @@ package com.blaife;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.ServletComponentScan;
+import org.springframework.context.ConfigurableApplicationContext;
 
 @SpringBootApplication
+// MapperScan mapper监视器 有了这个注释后mapper层不需要再写@mapper
 @MapperScan("com.blaife.mapper")
+// 监视所有@Component类 主要用于过滤器监听器 （session监听器目前还存在问题）
+// @ServletComponentScan(basePackages = "com.blaife.*")
 public class SpringbootExampleApplication {
 
     public static void main(String[] args) {
+
         SpringApplication.run(SpringbootExampleApplication.class, args);
+        
     }
 
 }
