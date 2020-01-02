8d404ca45e9ab715cfeeb3f49b9d56e053c5209b
BigBlackKnife <2767026270@qq.com>
2020-01-03 00:21:51 +0800

    request请求监听器提交

--- src/main/java/com/blaife/listener/RequestListenter.java
+++ src/main/java/com/blaife/listener/RequestListenter.java
@@ -1,5 +1,8 @@
 package com.blaife.listener;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 import javax.servlet.ServletRequestEvent;
 import javax.servlet.ServletRequestListener;
 import javax.servlet.annotation.WebListener;
@@ -7,8 +10,9 @@ import javax.servlet.annotation.WebListener;
 /**
  * request请求监听
  */
-@WebListener
 public class RequestListenter implements ServletRequestListener {
+
+    private final Logger logger = LoggerFactory.getLogger(RequestListenter.class);
 
     /**
      * 请求销毁
@@ -16,8 +20,7 @@ public class RequestListenter implements ServletRequestListener {
      */
     @Override
     public void requestDestroyed(ServletRequestEvent sre) {
-        System.out.println("---------------------------->请求销毁");
-
+        logger.info("this is RequestListenter:请求销毁");
     }
 
     /**
@@ -26,6 +29,6 @@ public class RequestListenter implements ServletRequestListener {
      */
     @Override
     public void requestInitialized(ServletRequestEvent sre) {
-        System.out.println("---------------------------->请求创建");
+        logger.info("this is RequestListenter:请求创建");
     }
 }
