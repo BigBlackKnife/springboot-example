9eb5dee24935a51796f8ad22c92a9b95756f0384
BigBlackKnife <2767026270@qq.com>
2020-01-06 00:06:03 +0800

    SpringTask定时任务测试类

---
+++ src/main/java/com/blaife/timetask/SchedulerTask.java
@@ -0,0 +1,41 @@
+package com.blaife.timetask;
+
+import org.springframework.scheduling.annotation.Async;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
+
+import java.time.LocalDateTime;
+
+/**
+ * 定时任务1
+ */
+@Component
+public class SchedulerTask {
+
+    /**
+     * 固定时间执行
+     * 一分钟执行一次
+     */
+    @Scheduled(cron="*0/6 * * * * * ")
+    @Async
+    public void process() {
+        try {
+            System.out.println("休眠开始");
+            Thread.sleep(10000);
+            System.out.println("休眠结束");
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        System.out.println("process：" + LocalDateTime.now());
+    }
+
+    /**
+     * 固定倒计时执行
+     * 六秒一次
+     */
+    @Scheduled(fixedRate = 6000)
+    @Async
+    public void reportCurrentTime() {
+        System.out.println("现在时间：" + LocalDateTime.now());
+    }
+}
