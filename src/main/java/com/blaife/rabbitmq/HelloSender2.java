7b83ec192c9423bb0c8451947a5ec92d6f31f033
BigBlackKnife <2767026270@qq.com>
2020-01-07 00:27:21 +0800

    Rabbit生产者消费者测试的方法提交

---
+++ src/main/java/com/blaife/rabbitmq/HelloSender2.java
@@ -0,0 +1,24 @@
+package com.blaife.rabbitmq;
+
+import org.springframework.amqp.core.AmqpTemplate;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+import java.util.Date;
+
+/**
+ * 生产者
+ */
+@Component
+public class HelloSender2 {
+
+    @Autowired
+    private AmqpTemplate rabbitTemplate;
+
+    public void send(int i) {
+        String context = "hello " + i + new Date();
+        System.out.println("Sender2 : " + context);
+        this.rabbitTemplate.convertAndSend("hello", context);
+    }
+
+}
