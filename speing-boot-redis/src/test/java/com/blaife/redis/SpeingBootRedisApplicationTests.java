1694d24a0cb0d4b2edd59997027adc2eacf1c61a
BigBlackKnife <2767026270@qq.com>
2020-01-12 00:31:11 +0800

    完全修改项目结构，springboot案例重新开始

---
+++ speing-boot-redis/src/test/java/com/blaife/redis/SpeingBootRedisApplicationTests.java
@@ -0,0 +1,13 @@
+package com.blaife.redis;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpeingBootRedisApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
