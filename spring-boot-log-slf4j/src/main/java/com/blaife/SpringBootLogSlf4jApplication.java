e657444dc267031ebc7c63bd453224ffa858307f
BigBlackKnife <2767026270@qq.com>
2020-02-15 20:25:46 +0800

    springboot-log-slf4j -- slf4j+logback日志案例提交

---
+++ spring-boot-log-slf4j/src/main/java/com/blaife/SpringBootLogSlf4jApplication.java
@@ -0,0 +1,13 @@
+package com.blaife;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootLogSlf4jApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootLogSlf4jApplication.class, args);
+    }
+
+}
